ceacfd01bd0e779231be4363ae0ce7c6426f7f01
nirajgeorgian <niraj.kishore@stayology.com>
2020-04-03 11:54:54 +0530

    added code for mongodb production

--- src/db/db.go
+++ src/db/db.go
@@ -2,10 +2,12 @@ package db
 
 import (
 	"context"
+	"time"
 
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // Database base type struct
@@ -13,19 +15,23 @@ type Database struct {
 	*mongo.Client
 }
 
+// New new mongodb database instanse
 func New(config *Config) (*Database, error) {
 	// Set client options
 	clientOptions := options.Client().ApplyURI(config.DatabaseURI)
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, err := mongo.Connect(ctx, clientOptions)
 
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to connect to database")
 	}
 
 	// Check the connection
-	err = client.Ping(context.TODO(), nil)
+	err = client.Ping(context.TODO(), readpref.Primary())
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to connect to database")
 	}
